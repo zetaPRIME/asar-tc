28b159c8ffd75ba7b9dc404a2b8da2405e073f19
zetaPRIME <qmanxt@gmail.com>
2016-01-29 21:20:26 -0500

    have a test file; make.sh -q doesn't compile library version

--- make.sh
+++ make.sh
@@ -5,10 +5,14 @@ if [[ "$OSTYPE" == 'msys' ]]; then
     echo Detected msys
     windres asar.rc -O coff -o asar.res
     g++ -std=c++98 -U__STRICT_ANSI__ -Wall -Werror -Wno-unused-result -Wno-uninitialized *.cpp -oasar.exe asar.res -s -O3 -fno-rtti -DINTERFACE_CLI -DFstricmp=strcasecmp -DGNUWIN32 -Dwindows -Dfixgw -DRELEASE
-    g++ -std=c++98 -U__STRICT_ANSI__ -Wall -Werror -Wno-unused-result -Wno-uninitialized *.cpp -olibasar.dll -s -O3 -fno-rtti -DINTERFACE_LIB -Dstricmp=strcasecmp -DGNUWIN32 -Dwindows -Dfixgw -DRELEASE -fvisibility=hidden -fvisibility-inlines-hidden -shared
+    if [[ "$1" != "-q" ]]; then
+        g++ -std=c++98 -U__STRICT_ANSI__ -Wall -Werror -Wno-unused-result -Wno-uninitialized *.cpp -olibasar.dll -s -O3 -fno-rtti -DINTERFACE_LIB -Dstricmp=strcasecmp -DGNUWIN32 -Dwindows -Dfixgw -DRELEASE -fvisibility=hidden -fvisibility-inlines-hidden -shared
+    fi
 else
     echo Detected linux
     #For all Linux users out there. I don't think anything uses the Asar library on Linux, but it does no harm.
     g++ -std=c++98 -Wall -Werror -Wno-unused-result -Wno-uninitialized *.cpp -oasar -s -O3 -fno-rtti -DINTERFACE_CLI -Dstricmp=strcasecmp -Dlinux -DRELEASE
-    g++ -std=c++98 -Wall -Werror -Wno-unused-result -Wno-uninitialized *.cpp -olibasar.so -s -O3 -fno-rtti -DINTERFACE_LIB -Dstricmp=strcasecmp -Dlinux -DRELEASE -fvisibility=hidden -fvisibility-inlines-hidden -shared
+    if [[ "$1" != "-q" ]]; then
+        g++ -std=c++98 -Wall -Werror -Wno-unused-result -Wno-uninitialized *.cpp -olibasar.so -s -O3 -fno-rtti -DINTERFACE_LIB -Dstricmp=strcasecmp -Dlinux -DRELEASE -fvisibility=hidden -fvisibility-inlines-hidden -shared
+    fi
 fi
---
+++ test/test.asm
@@ -0,0 +1,8 @@
+!x = 1
+!y = x
+print "!{!y}"
+if 0
+elseif !{!y}
+;if !{!y}
+print "1"
+endif
